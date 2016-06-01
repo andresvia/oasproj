6f03466d33dac5d5ef8c3091601413d694ca5064
andresvia <andres.via@gmail.com>
2016-06-01 00:56:31 -0500

    agregando configuracion de red por defecto en vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,9 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "centos/7"
+  config.vm.hostname = "drone.192.168.12.212.xip.io"
   config.vm.network "private_network", ip: "192.168.12.212"
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.provision "shell", inline: "rm -rf /tmp/target", run: "always"
   config.vm.provision "file", source: "target", destination: "/tmp/target", run: "always"
   config.vm.provision "shell", path: "scripts/vagrant-installer", run: "always"
--- template/vagrantfile.go
+++ template/vagrantfile.go
@@ -7,7 +7,9 @@ var vagrantfile = `# -*- mode: ruby -*-
 
 Vagrant.configure(2) do |config|
   config.vm.box = "centos/7"
+  # config.vm.hostname = "name.192.168.12.x.xip.io"
   # config.vm.network "private_network", ip: "192.168.12.x"
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.provision "shell", inline: "rm -rf /tmp/target", run: "always"
   config.vm.provision "file", source: "target", destination: "/tmp/target", run: "always"
   config.vm.provision "shell", path: "scripts/vagrant-installer", run: "always"
