c636d7dc610c2e018a3cea90a058929687dc937c
andresvia <andres.via@gmail.com>
2016-08-02 18:02:40 -0500

    systemd_unit_after ahora también afecta systemd unit requires

--- project/systemd.go
+++ project/systemd.go
@@ -3,8 +3,9 @@ package project
 func (p *Project) GetSystemdUnitOrder() (s string) {
 	if p.Systemd_unit_after != "" {
 		s = "After=" + p.Systemd_unit_after
+		s += "\nRequires=" + p.Systemd_unit_after
 	} else {
-		s = "After=network.service"
+		s = "After=network.target"
 	}
 	return
 }
